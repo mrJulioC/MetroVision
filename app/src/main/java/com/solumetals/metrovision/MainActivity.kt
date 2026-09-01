9dc6a05bf7a66696987e2f7db2c334568488c4d7
Julio Fernández <mrJulioC@users.noreply.github.com>
2026-08-31 19:15:12 -0500

    Aplicar proyecto MetroVision corregido

--- app/src/main/java/com/solumetals/metrovision/MainActivity.kt
+++ app/src/main/java/com/solumetals/metrovision/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Canvas
